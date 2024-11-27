691dbf2b8bde8013edacdd2874b3b83ae961fa46
'everythingfades' <'raymondguhonglei@163.com'>
2024-11-27 12:21:10 +0000

    Deployed 3bb72eb with MkDocs version: 1.6.1

--- assets/javascripts/decrypt-contents.js
+++ assets/javascripts/decrypt-contents.js
@@ -323,7 +323,7 @@ async function decryptor_reaction(key_or_keys, password_input, decrypted_content
 
         // any post processing on the decrypted content should be done here
         if (typeof MathJax === 'object') { MathJax.typesetPromise();};
-        
+        if (typeof mermaid === 'object') { mermaid.contentLoaded();};
         
         
         if (typeof theme_run_after_decryption !== 'undefined') {
